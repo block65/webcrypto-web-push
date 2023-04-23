512c5f998c8f2f87884bcb2dd96f062033526b0f
Max Holman <max@holmn.com>
2023-04-23 13:16:59 +0800

    chore: CryptoKey is exported separately and therefore this is redundant

--- lib/isomorphic-crypto.ts
+++ lib/isomorphic-crypto.ts
@@ -7,14 +7,13 @@ const impl = await (typeof globalThis !== 'undefined' && globalThis.crypto
   ? globalThis.crypto
   : import('node:crypto'));
 
-// we only export SubtleCrypto to make sure the interface remains the same
+// we only export the values we use to keep things simple, we dont need a fully
+// cross platform compatible crypto library
 export const crypto = {
   getRandomValues: <T extends Uint8Array>(array: T) =>
     'webcrypto' in impl
       ? impl.webcrypto.getRandomValues(array)
       : impl.getRandomValues(array),
-  CryptoKey:
-    'webcrypto' in impl ? impl.webcrypto.CryptoKey : globalThis.CryptoKey,
   subtle: 'webcrypto' in impl ? impl.webcrypto.subtle : impl.subtle,
 };
 
