69f2c88c8194fa594d62ae66d7c4ecd0bc1e2179
Max Holman <max@holmn.com>
2023-09-04 09:53:17 +0800

    chore: avoid snapshot discrepancy when running on devices with different timezones

--- test/notification.test.ts
+++ test/notification.test.ts
@@ -56,7 +56,7 @@ describe('', () => {
   });
 
   test('buildPushPayload', async () => {
-    vi.setSystemTime(new Date(2000, 1, 1, 13));
+    vi.setSystemTime(new Date(2000, 1, 1, 13, 0, 0, 0));
 
     const subscription = fakeSubscriptions.test;
 
