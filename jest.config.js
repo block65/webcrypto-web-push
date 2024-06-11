39af25558d77ef39b341c64a4ac901bb231f31df
Max Holman <max@holmn.com>
2024-06-11 11:48:55 +0800

    chore: use vitest

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  modulePathIgnorePatterns: ['<rootDir>/dist'],
-  testMatch: ['<rootDir>/__tests__/**/*.test.[jt]s?(x)'],
-  moduleNameMapper: {
-    '^(\\..*)\\.jsx?$': '$1', // support for ts imports with .js extensions
-  },
-  extensionsToTreatAsEsm: ['.ts', '.tsx'],
-};
