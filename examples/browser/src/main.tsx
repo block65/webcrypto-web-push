fcc37fc293d8627c3d12fd410ea01be895d6a173
Max Holman <max@holmn.com>
2024-12-15 15:47:28 +0800

    chore: lint

--- examples/browser/src/main.tsx
+++ examples/browser/src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.js';
 
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+const el = document.getElementById('root');
+
+if (el) {
+  createRoot(el).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
